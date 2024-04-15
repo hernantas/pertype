dc3190ba1de7f5fbc416a470f6c54fa6cdc73872
Zulfikar Raditya Hernanta <hernantas@gmail.com>
2024-04-15 14:09:23 +0700

    Add alias for object property key

--- packages/pertype/src/util/alias.ts
+++ packages/pertype/src/util/alias.ts
@@ -2,3 +2,8 @@
  * Alias for constructor of `T` type with `Args` parameters
  */
 export type Constructor<T, Args extends any[]> = new (...args: Args) => T
+
+/**
+ * Alias for valid object property key
+ */
+export type Key = string | number | symbol
