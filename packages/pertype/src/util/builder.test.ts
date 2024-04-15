951e83a2c0ed9d514354ce2dc0ff9a63d89af43f
Zulfikar Raditya Hernanta <hernantas@gmail.com>
2024-04-15 14:31:31 +0700

    Add `ImmutableBuilder` utility class

---
+++ packages/pertype/src/util/builder.test.ts
@@ -0,0 +1,25 @@
+import { AnyObject } from './alias'
+import { ImmutableBuilder } from './builder'
+
+describe('ImmutableBuilder', () => {
+  it('Should set the unknown value', () => {
+    const builder = new ImmutableBuilder({} as AnyObject).set('key', 'value')
+    expect(builder.get('key')).toBe('value')
+  })
+
+  it('Should overwrite the value with same key', () => {
+    const builder = new ImmutableBuilder({} as AnyObject)
+      .set('key', 'value')
+      .set('key', 'overwrite')
+    expect(builder.get('key')).toBe('overwrite')
+  })
+
+  it('Should create new instance each set', () => {
+    const builder1 = new ImmutableBuilder({} as AnyObject)
+    const builder2 = builder1.set('key', 'value')
+    const builder3 = builder2.set('key', 'overwrite')
+    expect(builder1).not.toBe(builder2)
+    expect(builder1).not.toBe(builder3)
+    expect(builder2).not.toBe(builder3)
+  })
+})
---
+++ packages/pertype/src/util/builder.ts
@@ -0,0 +1,19 @@
+import { AnyObject, Constructor } from './alias'
+
+export class ImmutableBuilder<T extends AnyObject> {
+  public constructor(
+    private readonly definition: T
+  ) {}
+
+  public get<K extends keyof T>(key: K): T[K] {
+    return this.definition[key]
+  }
+
+  public set<K extends keyof T>(key: K, value: T[K]): this {
+    const Ctor = this.constructor as Constructor<this, [T]>
+    return new Ctor({
+      ...this.definition,
+      [key]: value
+    })
+  }
+}
