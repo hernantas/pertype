86efdceb1532342c60ce2005a34f622f27bb0ab4
Zulfikar Raditya Hernanta <hernantas@gmail.com>
2024-05-07 23:05:17 +0700

    Add `Output` and `Input` type

--- packages/pertype/src/util/type.ts
+++ packages/pertype/src/util/type.ts
@@ -13,3 +13,23 @@ export type TypeOf<T> = T extends Type
   : {
       [K in keyof T]: TypeOf<T[K]>
     }
+
+export interface Output<T = any> {
+  readonly __output: T
+}
+
+export type OutputOf<T> = T extends Output
+  ? T['__output']
+  : {
+      [K in keyof T]: OutputOf<T[K]>
+    }
+
+export interface Input<T = any> {
+  readonly __input: T
+}
+
+export type InputOf<T> = T extends Input
+  ? T['__input']
+  : {
+      [K in keyof T]: InputOf<T[K]>
+    }
