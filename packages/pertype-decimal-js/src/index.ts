e9bacc767b713fd8bac7598f7ef99fb402ed7661
Zulfikar Raditya Hernanta <hernantas@gmail.com>
2024-05-16 14:25:05 +0700

    Add `DecimalSchema` class

---
+++ packages/pertype-decimal-js/src/index.ts
@@ -0,0 +1,37 @@
+import { Schema, UnsupportedTypeError, UnsupportedValueError } from 'pertype'
+import { Decimal } from 'decimal.js'
+
+export class DecimalSchema extends Schema<Decimal, string> {
+  private static readonly instance = new DecimalSchema({})
+
+  public static create(): DecimalSchema {
+    return this.instance
+  }
+
+  public override is(value: unknown): value is Decimal {
+    return value instanceof Decimal
+  }
+
+  public override decode(value: unknown): Decimal {
+    if (this.is(value)) {
+      return value
+    }
+
+    if (typeof value === 'string' || typeof value === 'number') {
+      try {
+        return new Decimal(value)
+      } catch {
+        throw new UnsupportedValueError(value)
+      }
+    }
+    throw new UnsupportedTypeError(value)
+  }
+
+  public override encode(value: Decimal): string {
+    return value.toString()
+  }
+}
+
+export function decimal(): DecimalSchema {
+  return DecimalSchema.create()
+}
