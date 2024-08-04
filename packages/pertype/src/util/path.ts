b51d2caab45a0b73e5d4d6d67f2058bd93f7fe98
Zulfikar Raditya Hernanta <hernantas@gmail.com>
2024-08-04 10:00:24 +0700

    Add jsdoc to utility type

--- packages/pertype/src/util/path.ts
+++ packages/pertype/src/util/path.ts
@@ -1,7 +1,13 @@
-export function resolvePath(...keys: (string | undefined)[]): string {
-  return keys
-    .filter((key): key is string => key !== undefined)
-    .flatMap((key) => key.split('.'))
-    .filter((key) => key !== '')
+/**
+ * Resolves a path by joining multiple path segments together.
+ *
+ * @param paths The path segments to join.
+ * @returns The resolved path.
+ */
+export function resolvePath(...paths: (string | undefined)[]): string {
+  return paths
+    .filter((path): path is string => path !== undefined)
+    .flatMap((path) => path.split('.'))
+    .filter((path) => path !== '')
     .join('.')
 }
--- packages/pertype/src/util/type.ts
+++ packages/pertype/src/util/type.ts
@@ -2,11 +2,14 @@
  * Utility type to store type that can be inferred
  */
 export interface Type<T = any> {
+  /**
+   * Type that can be inferred
+   */
   readonly __type: T
 }
 
 /**
- * Infer the represented type by the schema
+ * Recursively infer type from {@link Type}
  */
 export type TypeOf<T> = T extends Type
   ? T['__type']
@@ -14,22 +17,37 @@ export type TypeOf<T> = T extends Type
       [K in keyof T]: TypeOf<T[K]>
     }
 
+/**
+ * Infer stored type from {@link Type}
+ */
 export type Infer<T> = TypeOf<T>
 
+/**
+ * Utility type to store output type
+ */
 export interface Output<T = any> {
   readonly __output: T
 }
 
+/**
+ * Recursively infer output type from {@link Output}
+ */
 export type OutputOf<T> = T extends Output
   ? T['__output']
   : {
       [K in keyof T]: OutputOf<T[K]>
     }
 
+/**
+ * Utility type to store input type
+ */
 export interface Input<T = any> {
   readonly __input: T
 }
 
+/**
+ * Recursively infer input type from {@link Input}
+ */
 export type InputOf<T> = T extends Input
   ? T['__input']
   : {
