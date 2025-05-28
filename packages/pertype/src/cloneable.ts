a895d6f10d31fc79f6420c612974ad00bcb1cb8b
Zulfikar Raditya Hernanta <hernantas@gmail.com>
2025-05-28 13:18:38 +0700

    Refactor `Cloneable` docs

--- packages/pertype/src/cloneable.ts
+++ packages/pertype/src/cloneable.ts
@@ -2,19 +2,21 @@ import { MetaTarget } from './metadata'
 import { Constructor } from './util/alias'
 
 /**
- * Utility class to build a record object (key-value) that create new instance
- * every time its properties mutated
+ * Cloneable is a base class for record object (key-value object) that can be cloned.
+ * It provides a way to store and retrieve values using a key-value pair.
+ * Each time a value is set, a new instance of the class is created.
+ *
+ * @template T The type of the record object
  */
 export class Cloneable<T extends {}> implements MetaTarget {
   /**
    * NEVER OVERRIDE CONSTRUCTOR.
    *
    * This constructor is used each time the stored record object is mutated.
-   * Any changes to the parameter signature can cause unsafe parameters passed
-   * wrong.
+   * Any changes to the parameter signature can cause issues with the cloning process.
    *
    * @param definition A record object
-   * @param parent The parent to inherited metadata from
+   * @param parent Optional parent object that can be used to link to a parent object and can be used to inherit metadata
    */
   protected constructor(
     private readonly definition: T,
@@ -22,32 +24,32 @@ export class Cloneable<T extends {}> implements MetaTarget {
   ) {}
 
   /**
-   * Get value of store object given by the key
+   * Get value of given key stored on record object.
    *
    * @param key Key used for map
-   * @returns Value of given key stored on the store
+   * @returns Value of given key stored on the record object
    */
   public get<K extends keyof T>(key: K): T[K] {
     return this.definition[key]
   }
 
   /**
-   * Store given value with given key to the object store. Always create a new
-   * instance
+   * Set value for given key on record object. This method creates a new instance of the class with the updated value.
    *
-   * @param key Key of store object
-   * @param value Value for given key that will be stored
-   * @returns A new instance of current class
+   * @param key Key of record object
+   * @param value Value to set for given key
+   * @returns A new instance of the class with the updated value
    */
   public set<K extends keyof T>(key: K, value: T[K]): this {
     return this.clone({ ...this.definition, [key]: value })
   }
 
   /**
-   * Create a new instance of current object. Copy current definition by default
+   * Clone the current instance of the class. This method creates a new instance
+   * of the class with the same definition or a new definition if provided.
    *
    * @param newDefinition Optional new definition that will be used for new instance
-   * @returns A new instance of current object
+   * @returns A new instance of the class
    */
   public clone(newDefinition: T = this.definition): this {
     const Ctor = this.constructor as Constructor<
